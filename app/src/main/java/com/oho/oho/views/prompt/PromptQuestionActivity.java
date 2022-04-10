c2fe292bde5b210c78cbb20df798c5b0a7cc93f5
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2022-04-10 14:47:37 +0600

    Prompt Answer UI added

--- app/src/main/java/com/oho/oho/views/prompt/PromptQuestionActivity.java
+++ app/src/main/java/com/oho/oho/views/prompt/PromptQuestionActivity.java
@@ -1,6 +1,7 @@
 package com.oho.oho.views.prompt;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -10,14 +11,27 @@ import com.oho.oho.R;
 
 public class PromptQuestionActivity extends AppCompatActivity {
 
+    private View fabButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompt_question);
 
+        fabButton = findViewById(R.id.button_fab_prompt);
+
         Fragment fragment;
         fragment = new PromptListFragment();
         loadFragment(fragment);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment;
+                fragment = new PromptAnswerFragment();
+                loadFragment(fragment);
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment) {
