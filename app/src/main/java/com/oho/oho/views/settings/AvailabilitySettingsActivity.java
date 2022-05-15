3dfc336fa0981e2cfb4d8366c91d744cb5419835
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2022-05-16 03:10:52 +0600

    Availability Setting ui changed

--- app/src/main/java/com/oho/oho/views/settings/AvailabilitySettingsActivity.java
+++ app/src/main/java/com/oho/oho/views/settings/AvailabilitySettingsActivity.java
@@ -3,13 +3,14 @@ package com.oho.oho.views.settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.oho.oho.R;
 import com.oho.oho.databinding.ActivityAvailabilitySettingsBinding;
 import com.oho.oho.databinding.ActivityMainBinding;
 
-public class AvailabilitySettingsActivity extends AppCompatActivity {
+public class AvailabilitySettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityAvailabilitySettingsBinding binding;
 
@@ -21,13 +22,15 @@ public class AvailabilitySettingsActivity extends AppCompatActivity {
         binding = ActivityAvailabilitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.evening_time_slots_array, R.layout.spinner_availability_list);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        binding.spinnerFrom.setAdapter(adapter);
-        binding.spinnerTo.setAdapter(adapter);
+        //Click Listeners
+//        binding.buttonAddSlot2.setOnClickListener(this);
+//        binding.buttonAddSlot3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+        }
     }
 }
