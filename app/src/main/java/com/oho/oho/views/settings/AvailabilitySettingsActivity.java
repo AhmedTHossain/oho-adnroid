cd5b1d5b85ad9c0744fd339cf6c18f82fb8936c0
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2022-05-16 19:37:11 +0600

    AvailabilitySettingsActivity - onCheckboxClick added

--- app/src/main/java/com/oho/oho/views/settings/AvailabilitySettingsActivity.java
+++ app/src/main/java/com/oho/oho/views/settings/AvailabilitySettingsActivity.java
@@ -3,17 +3,22 @@ package com.oho.oho.views.settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.oho.oho.R;
 import com.oho.oho.databinding.ActivityAvailabilitySettingsBinding;
 import com.oho.oho.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class AvailabilitySettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityAvailabilitySettingsBinding binding;
+    ArrayList<String> selectedSlotsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,8 @@ public class AvailabilitySettingsActivity extends AppCompatActivity implements V
         binding = ActivityAvailabilitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        selectedSlotsArray = new ArrayList<>();
+
         //Click Listeners
         binding.buttonClearSlots.setOnClickListener(this);
         binding.buttonSaveSlots.setOnClickListener(this);
@@ -30,7 +37,7 @@ public class AvailabilitySettingsActivity extends AppCompatActivity implements V
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.button_clear_slots:
                 binding.friSlot1.setChecked(false);
                 binding.friSlot2.setChecked(false);
@@ -47,8 +54,40 @@ public class AvailabilitySettingsActivity extends AppCompatActivity implements V
         }
     }
 
-    private void saveAvailability(){
+    private void saveAvailability() {
         //Call update availability API
-        Toast.makeText(getApplicationContext(),"Your slots for the weekend has been saved!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Your slots for the weekend has been saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        String day = "";
+
+        switch (view.getId()) {
+            case R.id.fri_slot_1:
+            case R.id.fri_slot_2:
+            case R.id.fri_slot_3:
+                day = "Fri";
+                break;
+            case R.id.sat_slot_1:
+            case R.id.sat_slot_2:
+            case R.id.sat_slot_3:
+                day = "Sat";
+                break;
+            case R.id.sun_slot_1:
+            case R.id.sun_slot_2:
+                day = "Sun";
+                break;
+        }
+
+        String slot = day + ", " + ((CheckBox) view).getText().toString();
+        if (checked)
+            selectedSlotsArray.add(slot);
+        else
+            selectedSlotsArray.remove(slot);
+
+        Log.d("AvailabilitySettingsActivity","selected slots = "+selectedSlotsArray.toString());
     }
 }
