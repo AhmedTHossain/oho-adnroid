1e89400760ec742cd2cf08c27210b2fdc77bb945
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2022-06-20 02:01:10 +0600

    feat: develop new login screen ui

--- app/src/main/java/com/oho/oho/views/LoginActivity.java
+++ app/src/main/java/com/oho/oho/views/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -48,6 +49,11 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.Theme_OHO);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
 
         initSignInButton();
         initAuthViewModel();
