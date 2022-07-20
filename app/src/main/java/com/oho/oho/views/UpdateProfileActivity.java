7882dee8029a5cca845dc29e69ce83c257e85e8e
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2022-07-20 20:05:20 +0600

    feat: add bio edit ui in Update Profile screen

--- app/src/main/java/com/oho/oho/views/UpdateProfileActivity.java
+++ app/src/main/java/com/oho/oho/views/UpdateProfileActivity.java
@@ -3,6 +3,9 @@ package com.oho.oho.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 
 import com.oho.oho.R;
 import com.oho.oho.databinding.ActivityCompleteProfileBinding;
@@ -18,5 +21,24 @@ public class UpdateProfileActivity extends AppCompatActivity {
         binding = ActivityUpdateProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.edittextAbout.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length()>0)
+                    binding.buttonUpdateAbout.setVisibility(View.VISIBLE);
+                else
+                    binding.buttonUpdateAbout.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
