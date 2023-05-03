a19ec27d0f941cbffb65f2625dbf76d41c43bea1
Ahmed T Hossain <ahmedthossain@outlook.com>
2023-05-03 11:44:33 +0600

    feat: implement socket.io
    feat: connect to web socket with JWT auth token
    
    feat: start listening to various events

--- app/src/main/java/com/oho/oho/views/chat/ChatActivity.java
+++ app/src/main/java/com/oho/oho/views/chat/ChatActivity.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,7 +20,18 @@ import com.oho.oho.responses.ChatRoom;
 import com.oho.oho.viewmodels.ChatViewModel;
 import com.oho.oho.viewmodels.MessageViewModel;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import io.socket.engineio.client.transports.WebSocket;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -27,6 +39,21 @@ public class ChatActivity extends AppCompatActivity {
     private ChatRoom selectedChatRoom;
     private ChatViewModel viewModel;
     private ShimmerFrameLayout shimmerViewContainer;
+
+    private Socket mSocket;
+    {
+        try {
+            IO.Options options = new IO.Options();
+            options.auth = new HashMap<>();
+            options.auth.put("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODMxNzYzOTIsImlhdCI6MTY4MzA4OTk5Miwic3ViIjo5OX0.dneixvG-WuYMYYMf0Cm6sEhnWZBGKMTCWBjG2b5cs94");
+
+//            options.transports = new String[]{WebSocket.NAME};
+
+            mSocket = IO.socket("http://34.232.79.30:3000", options);
+        } catch (URISyntaxException e) {
+            Log.d("ChatActivity","socket exception: "+ e.getMessage());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +66,51 @@ public class ChatActivity extends AppCompatActivity {
         binding.screentitle.setText(selectedChatRoom.getFullName());
         shimmerViewContainer = binding.shimmerViewContainer;
         initChatViewModel();
+
+        mSocket.connect();
+
+        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d("ChatActivity","socket connected");
+            }
+        });
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Exception e = (Exception) args[0];
+                Log.d("ChatActivity","socket connection error: " + e.getCause());
+            }
+        });
+
+        mSocket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d("ChatActivity","socket dis-connected");
+            }
+        });
+
+        mSocket.on("227bc74c-5d49-4beb-8b70-1743a4b912e0", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d("ChatActivity","socket connection to chatroom: Successfull!");
+
+                String message = (String) args[0];
+                Log.d("ChatActivity", "Received message: " + message);
+            }
+        });
+
+//        mSocket.on("message", new Emitter.Listener() {
+//            @Override
+//            public void call(Object... args) {
+//                String message = (String) args[0];
+//                // Process the message here
+//                Log.d("ChatActivity", "Received message: " + message);
+//            }
+//        });
+
+
+//        mSocket.disconnect();
     }
 
     @Override
@@ -50,7 +122,14 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         binding.shimmerViewContainer.stopShimmerAnimation();
+        mSocket.disconnect();
         super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mSocket.disconnect();
+        super.onDestroy();
     }
 
     private void initChatViewModel() {
